9c828fac8469b5361310e161679836d17c7db0c1
Benjamin Fuller <benjamin.fuller@uconn.edu>
2021-04-21 12:06:39 -0400

    multi threading search

--- fhipe/prox_search.py
+++ fhipe/prox_search.py
@@ -18,50 +18,59 @@ PERFORMANCE OF THIS SOFTWARE.
 Implementation of Ahmad et al. Proximity Search Scheme 
 """
 
+import sys, os, math, random, time, zlib, secrets, dill, threading,time, concurrent.futures
 
-import sys, os, math, random, time, zlib, secrets
 # Path hack
 sys.path.insert(0, os.path.abspath('charm'))
 sys.path.insert(1, os.path.abspath('../charm'))
 
-from charm.toolbox.pairinggroup import PairingGroup,ZR,G1,G2,GT,pair
+from charm.toolbox.pairinggroup import PairingGroup, ZR, G1, G2, GT, pair
 from subprocess import call, Popen, PIPE
+
+from pathos.multiprocessing import ProcessingPool as Pool, cpu_count
+
+
+def match_item(decrypt_method, pub, index, ciphertext, token):
+    for subquery in token:
+        if decrypt_method(pub, ciphertext, subquery):
+            return index
+            break
+    return
 
 
 class ProximitySearch():
-    def __init__(self,  n, predicate_scheme, group_name = 'MNT159', simulated = False):
+    def __init__(self, n, predicate_scheme, group_name='MNT159', simulated=False):
         self.predinstance = predicate_scheme(n + 1, group_name, simulated)
         self.public_parameters = self.predinstance.getPublicParameters()
         self.vector_length = n
 
     def encrypt_dataset(self, data_set):
         for data_item in data_set:
-            if(len(data_item)!=self.vector_length):
+            if (len(data_item) != self.vector_length):
                 raise ValueError("Improper Vector Size")
 
         self.enc_data = {}
-        i=0
+        i = 0
         print(data_set)
         for x in data_set:
-            print("Value to encrypt is "+str(x))
-            x2 = [xi if xi==1 else -1 for xi in x]
+            print("Value to encrypt is " + str(x))
+            x2 = [xi if xi == 1 else -1 for xi in x]
             x2.append(-1)
-            print("Data to encrypt is "+str(x2))
+            print("Data to encrypt is " + str(x2))
             self.enc_data[i] = self.predinstance.encrypt(x2)
-            i= i+1
-
+            i = i + 1
 
     def generate_query(self, query, distance):
-        encoded_query = [xi if xi==1 else -1 for xi in query]
+        encoded_query = [xi if xi == 1 else -1 for xi in query]
         query_set = []
-        for i in range(distance+1):
+        for i in range(distance + 1):
             temp_query = list(encoded_query)
-            temp_query.append(self.vector_length-2*i)
+            temp_query.append(self.vector_length - 2 * i)
             query_set.append(temp_query)
 
-        print("Query set is "+str(query_set))
+        print("Query set is " + str(query_set))
         encrypted_query = []
-        while(len(query_set)>0):
+        while (len(query_set) > 0):
             next_to_encode = secrets.randbelow(len(query_set))
             encrypted_query.append(self.predinstance.keygen(query_set[next_to_encode]))
             query_set.remove(query_set[next_to_encode])
@@ -69,14 +78,18 @@ class ProximitySearch():
         return encrypted_query
 
     def search(self, query):
-        data_to_return=[]
-        for data_item in self.enc_data:
-            for subquery in query:
-                if self.predinstance.decrypt(self.public_parameters, self.enc_data[data_item], subquery):
-                    data_to_return.append(data_item)
-                    break
-
-        return data_to_return
-
-
-
+        result_list = []
+        with Pool(processes=cpu_count()) as p:
+            data_val_aug = [(self.predinstance.decrypt, self.public_parameters, x, self.enc_data[x], query) for x in
+                            self.enc_data]
+            with concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count()) as executor:
+                future_list = {executor.submit(match_item, self.predinstance.decrypt,
+                                               self.public_parameters, x, self.enc_data[x], query)
+                               for x in self.enc_data
+                               }
+                for future in concurrent.futures.as_completed(future_list):
+                    res = future.result()
+                    if res is not None:
+                        result_list.append(res)
+        return result_list
+    
